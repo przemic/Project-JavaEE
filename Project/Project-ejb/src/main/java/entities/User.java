9b5881383fb7a4c1c748653c89a8b57a60b69d43
Przemysław Michalak <przemic@yahoo.pl>
2012-04-19 20:57:47 +0200

    Added Entity Classes

---
+++ Project/Project-ejb/src/main/java/entities/User.java
@@ -0,0 +1,167 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Date;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author przemic
+ */
+@Entity
+@Table(name = "User", catalog = "java", schema = "")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "User.findAll", query = "SELECT u FROM User u"),
+    @NamedQuery(name = "User.findById", query = "SELECT u FROM User u WHERE u.id = :id"),
+    @NamedQuery(name = "User.findByBirthDate", query = "SELECT u FROM User u WHERE u.birthDate = :birthDate")})
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "id", nullable = false)
+    private Integer id;
+    @Lob
+    @Size(max = 65535)
+    @Column(name = "name", length = 65535)
+    private String name;
+    @Lob
+    @Size(max = 65535)
+    @Column(name = "surname", length = 65535)
+    private String surname;
+    @Column(name = "birth_date")
+    @Temporal(TemporalType.DATE)
+    private Date birthDate;
+    @Lob
+    @Size(max = 65535)
+    @Column(name = "password", length = 65535)
+    private String password;
+    @Lob
+    @Size(max = 65535)
+    @Column(name = "login", length = 65535)
+    private String login;
+    @JoinColumn(name = "Group_id", referencedColumnName = "id", nullable = false)
+    @ManyToOne(optional = false)
+    private Group groupid;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "userid")
+    private Collection<EventToUserAsoc> eventToUserAsocCollection;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "userid")
+    private Collection<Comment> commentCollection;
+
+    public User() {
+    }
+
+    public User(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public Group getGroupid() {
+        return groupid;
+    }
+
+    public void setGroupid(Group groupid) {
+        this.groupid = groupid;
+    }
+
+    @XmlTransient
+    public Collection<EventToUserAsoc> getEventToUserAsocCollection() {
+        return eventToUserAsocCollection;
+    }
+
+    public void setEventToUserAsocCollection(Collection<EventToUserAsoc> eventToUserAsocCollection) {
+        this.eventToUserAsocCollection = eventToUserAsocCollection;
+    }
+
+    @XmlTransient
+    public Collection<Comment> getCommentCollection() {
+        return commentCollection;
+    }
+
+    public void setCommentCollection(Collection<Comment> commentCollection) {
+        this.commentCollection = commentCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof User)) {
+            return false;
+        }
+        User other = (User) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ejb.User[ id=" + id + " ]";
+    }
+    
+}
